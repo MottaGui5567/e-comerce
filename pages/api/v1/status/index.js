c5bc6ed9dd4d532972be39240c5566894972a75f
MottaGui5567 <nssyyj87h7@privaterelay.appleid.com>
2026-06-04 17:02:06 +0000

    add jsconfig.json para pastas raiz

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,10 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1");
   console.log(result);
   response.status(200).json({
     cahve: "endpoint status",
-    result: [{ "?column?": 2 }],
   });
 }
 
