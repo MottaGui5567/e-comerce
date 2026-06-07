52010cfe76223f6613b8aa5a99379e68763637ea
MottaGui5567 <nssyyj87h7@privaterelay.appleid.com>
2026-06-07 19:18:11 +0000

    adicionado conexoes maximas e usadas na api juntamente com server version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result);
+  //obter ultima atualização do status
+  const UpdateAt = new Date().toISOString();
+
+  //obter conexões maximas
+  const MaxConections = await database.query(
+    "SELECT current_setting('max_connections')::int AS max_connections",
+  );
+  const maxconn = MaxConections.rows[0].max_connections;
+
+  //oberter conexões usadas
+  const usedConections = await database.query(
+    "SELECT count(*) AS used_connections FROM pg_stat_activity",
+  );
+  const usedconn = usedConections.rows[0].used_connections;
+
+  //obter versão do banco de dados
+  const version = await database.query("SHOW server_version");
+  const versrionNumber = version.rows[0].server_version;
+
   response.status(200).json({
-    cahve: "endpoint status",
+    update_at: UpdateAt,
+    dependencies: {
+      database: {
+        version: versrionNumber,
+        conexoes_max: maxconn,
+        conexoes_usadas: usedconn,
+      },
+    },
   });
+  console.log(maxconn);
+  console.log(usedconn);
+  console.log(versrionNumber);
 }
 
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at);
+  expect(responseBody.update_at).toEqual(parsedUpdateAt.toISOString());
 });
